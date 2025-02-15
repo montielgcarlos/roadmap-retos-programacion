6ecc855540764debd7bc265c28b632fb23547062
Carlos <montielg.carlos@gmail.com>
2025-02-15 07:55:57 -0600

    montielgcarlos.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/montielgcarlos.py
@@ -0,0 +1,26 @@
+#Estoy muy contento y motivado por iniciar el curso "Retos de Programación", espero poder aprender la lógica en varios lenguajes.
+#La URL oficial de python es: https://www.python.org/")
+
+#Comentario
+
+"""
+Comentarios 
+en 
+varias 
+lineas
+"""
+
+#Crear variables
+variable = 12
+type(variable)
+
+variable_2 = 3.1416
+type(variable_2)
+
+variable_3 = "La programación te permite automatizar diferentes actividades de la vida"
+type(variable_3)
+
+variable_4 = True
+type(variable_4)
+
+print("¡Hola, python!")
